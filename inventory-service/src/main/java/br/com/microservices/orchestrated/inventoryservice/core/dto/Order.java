9e1d3be7533d9bd0a5e7490f83062d43efe373ab
Francisco Viana <franciscosantos29@gmail.com>
2024-12-04 16:13:48 -0300

    Inventory-service: microsserviço de inventário/estoque

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/dto/Order.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/dto/Order.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class Order {
 
     private String id;
-    private List<OrderProducts> orderProducts;
+    private List<OrderProducts> products;
     private LocalDateTime createdAt;
     private String transactionId;
     private double totalAmount;
